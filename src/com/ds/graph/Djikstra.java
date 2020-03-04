cb03ef6bab9b9edc631f6ffa79f752c09ffa9ada
puperfused <abhi9651kr@gmail.com>
2020-03-05 00:37:37 +0530

    Adding Graph Codes

--- src/com/ds/graph/Djikstra.java
+++ src/com/ds/graph/Djikstra.java
@@ -1,11 +1,31 @@
 package com.ds.graph;
 
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.*;
+
+class Node implements Comparator<Node> {
+    public int node;
+    public int cost;
+
+    public Node(int node, int cost) {
+        this.node = node;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compare(Node node1, Node node2) {
+        if(node1.cost < node2.cost) {
+            return -1;
+        }
+        if(node1.cost > node2.cost) {
+            return 1;
+        }
+        return 0;
+    }
+}
 
 class GraphShortestPath{
     int V;
-    LinkedList<Integer> adj[];
+    LinkedList<Node> adj[];
     int dist[];
     boolean visited[];
 
@@ -19,9 +39,9 @@ class GraphShortestPath{
         dist[0]=0;
     }
 
-    void addEdge(int u, int v) {
-        adj[u].add(v);
-        adj[v].add(u);
+    void addEdge(int u, int v, int w) {
+        adj[u].add(new Node(v,w));
+        adj[v].add(new Node(u,w));
     }
 
     int printDistance(int destinationNode) {
@@ -31,12 +51,24 @@ class GraphShortestPath{
         return this.dist[destinationNode];
     }
 
-     int shortestPathUtil(){
-
-    }
-
-    int shortestPath(int source) {
-
+    void shortestPath(int source) {
+        Queue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(source, 0));
+        visited[source] = true;
+        while(!pq.isEmpty()) {
+            Node temp = pq.poll();
+            int x = temp.node;
+            int w = temp.cost;
+            for(int i=0; i<adj[x].size(); i++) {
+                int y = adj[x].get(i).node;
+                int we = adj[x].get(i).cost;
+                if(!visited[y] && dist[y] < we + dist[x]) {
+                    dist[y] = we + dist[x];
+                    visited[y] = true;
+                    pq.add(new Node(y, dist[y]));
+                }
+            }
+        }
     }
 }
 
@@ -45,13 +77,14 @@ public class Djikstra {
         Scanner scan = new Scanner(System.in);
         int edge = scan.nextInt();
         GraphShortestPath graph = new GraphShortestPath(edge);
-        int u, v;
+        int u, v, w;
         for(int i=0; i<edge; i++) {
             u = scan.nextInt();
             v = scan.nextInt();
-            graph.addEdge(u,v);
+            w = scan.nextInt();
+            graph.addEdge(u,v,w);
         }
-        int result = graph.shortestPath(0);
+        graph.shortestPath(0);
         int destination = scan.nextInt();
         System.out.println("Shortest distance from node 0 " + graph.printDistance(destination));
     }
